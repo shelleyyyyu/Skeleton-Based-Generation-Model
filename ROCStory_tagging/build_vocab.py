c9e16dcec666383e23fafd41dffbf4d76e5c4e6c
Shelly Yu <shellyyu@cherrypicks.com>
2019-08-28 20:18:57 +0800

    add create dataset methods

---
+++ ROCStory_tagging/build_vocab.py
@@ -0,0 +1,49 @@
+import csv
+import spacy
+from tqdm import tqdm
+from tqdm._tqdm import trange
+
+
+def load_csv(filename):
+    with open(filename, 'r') as File:
+        stories = []
+        lines = csv.reader(File)
+        for line in lines:
+            stories.append(line[1:])
+    return stories[1:] 
+
+def merge_data(file1, file2):
+    data1 = load_csv(file1)
+    data2 = load_csv(file2)
+    merge_data = data1 + data2
+    return merge_data
+
+def wordcount(strl_ist):
+	count_dict = {}
+	for str in strl_ist:
+		if str in count_dict.keys():
+			count_dict[str] = count_dict[str] + 1
+		else:
+			count_dict[str] = 1
+		count_list = sorted(count_dict.items(),key=lambda x:x[1],reverse=True)
+	return count_list
+
+
+data = merge_data("./data/2016.csv","./data/2017.csv")
+
+words = []
+nlp = spacy.load('en')
+print("----------------HERE 1----------------")
+for stories in data:
+	for s in stories:
+		tokenized_data = nlp(s)
+		for token in tokenized_data:
+			words.append(token.string)
+print("----------------HERE 2----------------")
+
+count_dict = wordcount(words)
+print("----------------HERE 3----------------")
+
+with open("./new_data/vocab.txt", 'w') as file:
+	for c in count_dict:
+		file.write(str(c[0]) + " " + str(c[1]) +'\n')
---
+++ ROCStory_tagging/pos_tagging.py
@@ -0,0 +1,114 @@
+#{
+#	"pos tagging": ["DET", "NOUN", "ADP", "DET", "NOUN", "VERB", "DET", "NOUN", "NOUN", "PUNCT"],
+#	"dependency": ["det", "nsubj", "prep", "det", "pobj", "ROOT", "det", "compound", "dobj", "punct"], 
+#	"compression": ["unknownnnnnnnnnnn"], 
+#	"label": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+#	"text": ["the", "person", "at", "the", "office", "signed", "the", "marriage", "certificate", "."]
+#}
+
+
+#python -m spacy.en.download all
+import csv
+import spacy
+
+def load_csv(filename):
+    with open(filename, 'r') as File:
+        stories = []
+        lines = csv.reader(File)
+        for line in lines:
+            stories.append(line[1:])
+    return stories[1:] 
+
+def merge_data(file1, file2):
+    data1 = load_csv(file1)
+    data2 = load_csv(file2)
+    merge_data = data1 + data2
+    return merge_data
+
+def clean_data(data):
+	tag_data = []
+	story_data = []
+	for d in data:
+		tag_data.append(d[0])
+		story_data.append(" ".join(d[1:]))
+	return tag_data, story_data
+
+def construct_new_dataset(data):
+	nlp = spacy.load('en')
+	array = []
+	for d in data:
+		tokenized_data = nlp(d)
+		data_dict = {}
+		pos_tagging = []
+		dependency = []
+		compression = ["unknownnnnnnnnnnn"]
+		label = []
+		text = []
+		for token in tokenized_data:
+			label.append(0)
+			text.append(token.string.lower())
+			dependency.append(token.dep_)
+			pos_tagging.append(token.pos_)
+		data_dict["pos tagging"] = pos_tagging
+		data_dict["dependency"] = dependency
+		data_dict["compression"] = compression
+		data_dict["label"] = label
+		data_dict["text"] = text
+		array.append(data_dict)
+	return array
+
+nlp = spacy.load('en')
+data = merge_data("./data/2016.csv","./data/2017.csv")
+tag_data, story_data = clean_data(data)
+
+# skeleton
+array = construct_new_dataset(tag_data)
+#story_data = story_data[:10]
+
+train_lens = int(len(array) * 0.8)
+valid_lens = int(len(array) * 0.1)
+
+train_sc_array = array[:train_lens]
+valid_sc_array = array[train_lens:train_lens+valid_lens]
+test_sc_array = array[train_lens+valid_lens:]
+
+train_story_array = story_data[:train_lens]
+valid_story_array = story_data[train_lens:train_lens+valid_lens]
+test_story_array = story_data[train_lens+valid_lens:]
+
+with open ("./new_data/train_sc.txt", 'w') as file:
+	for a in train_sc_array:
+		file.write(str(a) + "\n")
+with open ("./new_data/valid_sc.txt", 'w') as file:
+	for a in valid_sc_array:
+		file.write(str(a) + "\n")
+with open ("./new_data/test_sc.txt", 'w') as file:
+	for a in test_sc_array:
+		file.write(str(a) + "\n")
+
+
+with open ("./new_data/train_process.txt", 'w') as file:
+	for a in train_story_array:
+		tokenized_data = nlp(a)
+		out_str = ""
+		for t in tokenized_data:
+			out_str += t.string.lower()
+		file.write(out_str + "\n")
+
+with open ("./new_data/valid_process.txt", 'w') as file:
+	for a in valid_story_array:
+		tokenized_data = nlp(a)
+		out_str = ""
+		for t in tokenized_data:
+			out_str += t.string.lower()
+		file.write(out_str + "\n")
+
+with open ("./new_data/test_process.txt", 'w') as file:
+	for a in test_story_array:
+		tokenized_data = nlp(a)
+		out_str = ""
+		for t in tokenized_data:
+			out_str += t.string.lower()
+		file.write(out_str + "\n")
+
+
